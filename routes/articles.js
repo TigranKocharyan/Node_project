15fd6821775390414595c55b3d8f9049e068883e
Tigran <tiko.kocharyan25@gmail.com>
2022-04-27 23:19:10 +0400

    Some_changes

--- routes/articles.js
+++ routes/articles.js
@@ -37,7 +37,7 @@ router.post('/', async (req, res, next) => {
     return async (req, res) => {
       let article = req.article
       article.title = req.body.title
-      article.description = req.body.description
+      article.content = req.body.description
       article.markdown = req.body.markdown
       try {
         article = await article.save()
